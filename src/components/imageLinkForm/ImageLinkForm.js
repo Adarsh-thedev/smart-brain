869b0cceaa619e450987fcf7bf8c262c6144500f
Adarsh-thedev <adarshpittu@gmail.com>
2020-01-31 01:53:21 +0530

    some styles added

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return(
@@ -6,9 +7,11 @@ const ImageLinkForm = () => {
             <p className = 'f3'>
                 {`This magic brain will detect faces in your picture, give it a try`}
             </p>
-            <div>
-                <input type = 'search' className = 'pa2 f4 w-70 center ' />
-                <button className = 'w-30'>Detect</button>
+            <div className = 'center'>
+                <div className = 'pa4 br4 shadow-5 center form grow'>
+                    <input type = 'search' className = 'bg-transparent hover-bg-black hover-white pa2 f4 w-70 center br3' />
+                    <button className = 'w-30 grow br3 f4 ml2 link ph3 pb2 dib white bg-light-purple'>Detect</button>
+                </div>
             </div>
         </div>
     );
