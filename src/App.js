24a1b403797db0a30699e1b75bd8454308ae3326
Adarsh-thedev <adarshpittu@gmail.com>
2020-02-01 12:04:09 +0530

    added particles.js to the bg, rank component created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import React, {Component} from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlOptions = {
+  particles : {
+    number : {
+      value : 100,
+      density : {
+        enable : true,
+        value_area : 800
+      },
+      line_linked : {
+        enable : true,
+        distance : 250,
+        width : 1
+      },
+      move : {
+        enable : true,
+        speed : 6
+      }
+    }
+  },
+  interactivity:{
+    detect_on:'canvas',
+    events:{
+        onhover:{
+          enable:true,
+          mode:'grab'
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className = 'particles' params={particlOptions}/>
         <Navigation/>
         <Logo/>
+        <Rank/>
         <ImageLinkForm/>
       </div>
     );
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -8,9 +8,9 @@ const ImageLinkForm = () => {
                 {`This magic brain will detect faces in your picture, give it a try`}
             </p>
             <div className = 'center'>
-                <div className = 'pa4 br4 shadow-5 center form grow'>
-                    <input type = 'search' className = 'bg-transparent hover-bg-black hover-white pa2 f4 w-70 center br3' />
-                    <button className = 'w-30 grow br3 f4 ml2 link ph3 pb2 dib white bg-light-purple'>Detect</button>
+                <div className = 'pa4 br4 shadow-5 center form'>
+                    <input type = 'search' className = 'bg-washed-yellow hover-bg-black hover-white pa2 f4 w-70 center br3' />
+                    <button className = 'w-30 grow pointer br3 f4 ml2 link ph3 pb2 dib white bg-light-purple'>Detect</button>
                 </div>
             </div>
         </div>
