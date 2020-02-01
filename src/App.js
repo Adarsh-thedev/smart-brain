00323ffb639aa6147ae8c3b9885cbb7ef07031f0
Adarsh-thedev <adarshpittu@gmail.com>
2020-02-02 00:54:05 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -60,6 +60,21 @@ const particlOptions = {
 
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input : ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {
+    console.log('working');
+  }
+
   render() {
     return (
       <div className="App">
@@ -67,7 +82,9 @@ class App extends Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm onInputChange = {this.onInputChange}
+          onButtonSubmit = {this.onSubmit}
+        />
       </div>
     );
   }
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
             <p className = 'f3'>
@@ -9,8 +9,16 @@ const ImageLinkForm = () => {
             </p>
             <div className = 'center'>
                 <div className = 'pa4 br4 shadow-5 center form'>
-                    <input type = 'search' className = 'bg-lightest-blue hover-bg-black hover-white pa2 f4 w-70 center br3' />
-                    <button className = 'w-30 tc grow pointer br3 f4 ml2 link ph3 pb2 dib white btn'>Detect</button>
+                    <input type = 'search' 
+                        className = 'bg-lightest-blue hover-bg-black hover-white pa2 f4 w-70 center br3' 
+                        onChange = {onInputChange}
+                    />
+                    <button 
+                        className = 'w-30 tc grow pointer br3 f4 ml2 link ph3 pb2 dib white btn'
+                        onClick = {onButtonSubmit}
+                    >
+                        Detect
+                    </button>
                 </div>
             </div>
         </div>
